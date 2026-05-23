3da03a3ad9296c29160759f9811abcde206c68e9
danyuanruci88888 <danyuanruci8@gmail.com>
2026-05-23 17:48:23 +0800

    Update week01_day03 homework

---
+++ homework/week01_day03/task_storage.py
@@ -0,0 +1,30 @@
+import json 
+
+def load_tasks():
+    try:
+        with open("tasks.json","r",encoding="utf-8") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+
+def save_tasks(tasks):
+    with open("tasks.json","w",encoding="utf-8") as f :
+        json.dump(tasks,f,ensure_ascii=False,indent=2)
+
+def add_task(tasks,title):
+    task={
+        "title":title,
+        "status":"todo"
+    }
+    tasks.append(task)
+
+def show_tasks(tasks):
+    for task in tasks:
+        print(task["title"],task["status"])
+
+tasks = load_tasks()
+
+add_task(tasks,"学习JSON文件保存")
+save_tasks(tasks)
+show_tasks(tasks)
+print("保存完成")
